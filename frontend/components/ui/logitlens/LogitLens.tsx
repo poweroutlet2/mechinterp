aa861c0aaa9c672cef17c8e167102f09a5d0d23c
root <harshp2015m@gmail.com>
2025-07-17 00:25:45 -0400

    make api url envar public

--- frontend/components/ui/logitlens/LogitLens.tsx
+++ frontend/components/ui/logitlens/LogitLens.tsx
@@ -13,7 +13,7 @@ import Heatmap from "./Heatmap";
 import { Separator } from "../separator";
 import { Textarea } from "../textarea";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 
 export interface LogitLensLayer {
 	hook_name: string;
